f42d54dcd9d596c56badcf22a2f225055a01e68b
IgnatP <ipomazkov@icloud.com>
2016-05-19 11:33:05 +0300

    additions19-05-commit-message

--- app/src/main/java/com/ivpomazkov/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/ivpomazkov/beatbox/BeatBoxFragment.java
@@ -79,7 +79,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
+        setRetainInstance(true);
         mBeatBox = new BeatBox(getActivity());
     }
 
